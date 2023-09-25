5aa561f5355d1c3879bb8d0a5a328e4c3789014d
checkiamsiam <issiam02415@gmail.com>
2023-09-25 21:36:06 +0600

    header making start

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/common/Header";
+import Header from "@/components/common/Header/Header";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
