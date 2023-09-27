dfeaa366df4f2cd9b66c5d91caac45ccdf76535c
checkiamsiam <issiam02415@gmail.com>
2023-09-27 20:59:03 +0600

    search bar

--- src/components/common/Header/SearchBar.tsx
+++ src/components/common/Header/SearchBar.tsx
@@ -1,14 +1,44 @@
+import { Box, Card, ClickAwayListener } from "@mui/material";
+import { useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { Search, SearchIconWrapper, StyledInputBase } from "./StyleComponents";
 
 const SearchBar = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClickAway = () => {
+    setOpen(false);
+  };
   return (
-    <Search>
-      <SearchIconWrapper>
-        <BiSearchAlt style={{ fontSize: "20px" }} />
-      </SearchIconWrapper>
-      <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
-    </Search>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <Search>
+        <SearchIconWrapper>
+          <BiSearchAlt style={{ fontSize: "20px" }} />
+        </SearchIconWrapper>
+        <StyledInputBase onFocus={handleOpen} placeholder="Search…" inputProps={{ "aria-label": "search" }} />
+        {open && (
+          <Card
+            sx={{
+              position: "absolute",
+              top: "100%",
+              left: 0,
+              width: "100%",
+              height: "300px",
+              bgcolor: "white",
+              color: "text.primary",
+              borderRadius: "5px",
+              marginTop: "5px",
+            }}
+          >
+            <Box sx={{ p: 2 }}>
+              <p>Search Suggestions</p>
+            </Box>
+          </Card>
+        )}
+      </Search>
+    </ClickAwayListener>
   );
 };
 
